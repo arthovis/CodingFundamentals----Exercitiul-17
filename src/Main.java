77189fbcd5a836d9e006ee7e9d113d1b374856eb
Andrei <andreifbaicu@gmail.com>
2019-06-09 15:24:57 +0300

    Added tries counter

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int guess;
         int numberToguess = new Random().nextInt(1000);
+        int count=0;
 
         do {
             System.out.println("Introduceti un numar intre 1 si 1000:");
             guess=scanner.nextInt();
+            count++;
              if (guess<numberToguess){
                 System.out.println("Numarul este prea mic! Mai incearca!");
             } else if (guess>numberToguess){
@@ -19,8 +21,7 @@ public class Main {
         }while (guess!=numberToguess);
 
 
-        System.out.println("Ai ghicit! Numarul pe care trebuia sa il ghicesti este: "+numberToguess);
-
+        System.out.println("Ai ghicit! Numarul pe care trebuia sa il ghicesti este: "+numberToguess+". Ai ghicit din "+count+" incercari.");
 
     }
 }
