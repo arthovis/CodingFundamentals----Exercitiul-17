a895d3d996abbd92272d6920ba851696f3b96a7c
Andrei <andreifbaicu@gmail.com>
2019-06-09 15:13:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int guess;
+        int numberToguess = new Random().nextInt(1000);
+
+        do {
+            System.out.println("Introduceti un numar intre 1 si 1000:");
+            guess=scanner.nextInt();
+             if (guess<numberToguess){
+                System.out.println("Numarul este prea mic! Mai incearca!");
+            } else if (guess>numberToguess){
+                System.out.println("Numarul este prea mare! Mai incearca");
+            }
+
+        }while (guess!=numberToguess);
+
+
+        System.out.println("Ai ghicit! Numarul pe care trebuia sa il ghicesti este: "+numberToguess);
+
+
+    }
+}
